59123ea7ce190da1fd83f3d0b40771a755f1173d
Tristan <thrawst88@gmail.com>
2021-07-17 17:53:31 -0400

    respect pixel scaling, removed unused NSBitmapImageRep

--- src/osx/prtscn_osx.m
+++ src/osx/prtscn_osx.m
@@ -27,31 +27,30 @@ public:
 bool allocated = false;
 UInt8 *data = NULL;
 
-// CGImageRef转换为NSData
-NSData * CGimageRefToNSData(CGImageRef imageRef)
-{
-	CFMutableDataRef dataRef = CFDataCreateMutable(NULL, 0);
-	CGImageDestinationRef destinationRef = CGImageDestinationCreateWithData(dataRef, kUTTypePNG, 1, NULL);
-	CGImageDestinationAddImage(destinationRef, imageRef, nil);
-	CGImageDestinationFinalize(destinationRef);
-	NSData *data = [NSData dataWithData:(NSData *)dataRef];
-	CFRelease(destinationRef);
-	CFRelease(dataRef);
-	return data;
-}
-
 IData getScreen(const int x, const int y, const int width, const int height)
 {
 	CGRect rect = CGRectMake(x, y, width, height);
-	CGImageRef imageRef = CGDisplayCreateImageForRect(CGMainDisplayID(), rect);
+
+	CFArrayRef window_list = CGWindowListCreate(
+		kCGWindowListOptionOnScreenOnly,
+    kCGNullWindowID
+	);
+	struct cf_object_releaser window_list_releaser(window_list);
+
+	// CGImageRef imageRef = CGDisplayCreateImageForRect(CGMainDisplayID(), rect);
+	CGImageRef imageRef = CGWindowListCreateImageFromArray(
+		rect,
+    window_list,
+		kCGWindowImageNominalResolution
+	);
 
 	NSBitmapImageRep * ns_image = [[NSBitmapImageRep alloc] initWithCGImage: imageRef];
 	struct ns_object_releaser ns_image_releaser(ns_image);
-	NSColorSpace *ns_cs_sRGB = [NSColorSpace sRGBColorSpace];
+	NSColorSpace *targetColorSpace = [NSColorSpace sRGBColorSpace];
 
 	const size_t src_width = CGImageGetWidth(imageRef);
 	const size_t src_height = CGImageGetHeight(imageRef);
-	const CGColorSpaceRef src_colorspace = CGImageGetColorSpace(imageRef);
+	const CGColorSpaceRef sourceColorSpace = CGImageGetColorSpace(imageRef);
 
 	unsigned int length = src_height * src_width * 4;
 	if (!allocated) {
@@ -61,16 +60,12 @@ IData getScreen(const int x, const int y, const int width, const int height)
 
 	@autoreleasepool
 	{
-	NSBitmapImageRep * ns_image_sRGB = [ns_image \
-		bitmapImageRepByConvertingToColorSpace: ns_cs_sRGB
-		renderingIntent: NSColorRenderingIntentDefault];
-
 	size_t i = 0;
 	for( size_t y = 0; y < src_height; ++y )
 	{
 		for( size_t x = 0; x < src_width; ++x, i += 4)
 		{
-			NSColor * color = [ns_image_sRGB colorAtX: x y: y];
+			NSColor * color = [ns_image colorAtX: x y: y];
 
 			CGFloat color_values[4] = {};
 			color_values[0] = [color redComponent  ];
@@ -78,15 +73,15 @@ IData getScreen(const int x, const int y, const int width, const int height)
 			color_values[2] = [color blueComponent ];
 			color_values[3] = [color alphaComponent];
 
-			CGColorRef cg_color = CGColorCreate(src_colorspace, color_values);
+			CGColorRef cg_color = CGColorCreate(sourceColorSpace, color_values);
 			struct cf_object_releaser cg_color_releaser(cg_color);
 
 			NSColor *fixed_color = [[NSColor colorWithCGColor: cg_color]
-																colorUsingColorSpace: ns_cs_sRGB];
+																colorUsingColorSpace: targetColorSpace];
 
-			data[i + 0] = (UInt8) ([fixed_color redComponent  ] * 255);
-			data[i + 1] = (UInt8) ([fixed_color greenComponent] * 255);
-			data[i + 2] = (UInt8) ([fixed_color blueComponent ] * 255);
+			data[i + 0] = (UInt8) lroundf([fixed_color redComponent  ] * 255);
+			data[i + 1] = (UInt8) lroundf([fixed_color greenComponent] * 255);
+			data[i + 2] = (UInt8) lroundf([fixed_color blueComponent ] * 255);
 			data[i + 3] = 255;
 		}
 	}
